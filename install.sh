53715b761afb6a101f546488888e29788ca53a77
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2019-08-04 14:25:55 +0000

    Edit for all Unix system

--- install.sh
+++ install.sh
@@ -2,8 +2,12 @@
 cd
 apt install python3
 apt install python3-pip
+pkg install python3
+pkg install python3-pip
+apt-get install python3
+apt-get install python3-pip
 pip3 install requests
-echo alias callspam="'cd && python3 Call-Spammer/spammer.py'">>.bashrc
+echo alias callspam="'cd && python3 callspam/spammer.py'">>.bashrc
 echo "\033[37;1;42m G O O D ! \033[0m"
 cd
-python3 Call-Spammer/spammer.py -h
+python3 callspam/spammer.py -h
