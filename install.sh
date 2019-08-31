2f87b858588b09224d436e376f6c3b2a2189936e
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2019-08-31 19:49:11 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,9 @@
 cd
 apt install python3
 apt install python3-pip
-pkg install python3
-pkg install python3-pip
-apt-get install python3
-apt-get install python3-pip
 pip3 install requests
-echo alias callspam="'cd && python3 callspam/spammer.py'">>.bashrc
+pip3 install colorama
+cho alias callspam="'cd && python3 callspam/spammer.py'">>.bashrc
 echo "\033[37;1;42m G O O D ! \033[0m"
 cd
 python3 callspam/spammer.py -h
