0bf9d9642fa180834ad33df197f67b438ace4ea1
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2019-09-01 21:05:39 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ apt install python3
 apt install python3-pip
 pip3 install requests
 pip3 install colorama
-echo clear>.bashrc
+echo clear>>.bashrc
 echo alias callspam-update="'clear && cd && rm -rf callspam && git clone https://github.com/FSystem88/callspam && clear && sh callspam/install.sh'">>.bashrc
 echo alias callspam="'clear && cd && python3 callspam/spammer.py'">>.bashrc
 clear
