38c55ddb60a977a6389d35417dbf3aac4684e269
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2019-09-01 18:03:55 +0000

    Fix install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ apt install python3-pip
 pip3 install requests
 pip3 install colorama
 echo clear>.bashrc
-echo alias callspam-update="'clear && cd && rm -rf spymer && git clone https://github.com/FSystem88/spymer && clear && sh spymer/install.sh'">>.bashrc
-echo alias callspam="'clear && cd && python3 spymer/spammer.py'">>.bashrc
+echo alias callspam-update="'clear && cd && rm -rf callspam && git clone https://github.com/FSystem88/callspam && clear && sh callspam/install.sh'">>.bashrc
+echo alias callspam="'clear && cd && python3 callspam/spammer.py'">>.bashrc
 clear
 cd
-python3 spymer/spammer.py
+python3 callspam/spammer.py
